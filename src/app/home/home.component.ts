ca11cbf2bb6770220097af74cd4f12006d1a079e
Helio <helio.miranda.n@gmail.com>
2024-07-02 21:31:38 -0300

    Ajustes e criação da view home onde exibirá a tela para acesso do aluno.

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Aluno } from '../models/aluno.model';
+import { TableModule } from 'primeng/table';
+import { CommonModule, DatePipe } from '@angular/common';
+import { DataFormatadaPipe } from '../pipes/data-formatada.pipe';
+import { AlunoService } from '../services/aluno.service';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    TableModule,
+    CommonModule,
+    DatePipe,
+    DataFormatadaPipe
+  ],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+  alunos: Aluno[] = [];
+  constructor (public alunoService: AlunoService) {
+
+  }
+
+  ngOnInit(): void {
+    this.alunoService.getAluno().subscribe(response => {
+      this.alunos = response
+      console.log(this.alunos)
+    })
+  }
+}
