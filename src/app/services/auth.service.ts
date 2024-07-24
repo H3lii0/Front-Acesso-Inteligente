86dbd11bca0da8112c7a8979a54ac42a3da1871f
Helio <helio.miranda.n@gmail.com>
2024-07-23 21:44:16 -0300

    feat: Criação do guard e service que fará a parte de autenticação do login

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,56 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+ 
+  private readonly JWT_TOKEN = 'JWT_TOKEN';
+  private usuarioLogado?: string;
+  private autenticado = new BehaviorSubject<boolean>(false);
+  private router = inject(Router);
+  private http = inject(HttpClient);
+  private apiUrl = 'http://127.0.0.1:8000/api/login';
+
+  constructor(
+    // private http: HttpClient,
+
+  ) { }
+
+  login (user: {
+    email: string, password: string
+  }): Observable<any> {
+    return this.http.post<any>(this.apiUrl, user).pipe(
+      tap((response: any) => this.usuarioLogar(user.email, response.token))
+    );
+  }
+
+  private usuarioLogar (email: string, token: any) {
+    this.usuarioLogado = email;
+    this.salvarToken(token);
+    this.autenticado.next(true);
+  }
+
+  private salvarToken(jwt: string): void {
+    localStorage.setItem(this.JWT_TOKEN, jwt)
+  }
+
+  infoUsuarioAtual () {
+    return this.http.get(this.apiUrl)
+  }
+
+  logado () {
+    return !!localStorage.getItem(this.JWT_TOKEN)
+  }
+
+  logout () {
+    localStorage.removeItem(this.JWT_TOKEN);
+    this.autenticado.next(false);
+    this.router.navigate(['/login']);
+  }
+
+
+}
