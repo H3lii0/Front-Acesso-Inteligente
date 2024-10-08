fc12e6da4678b9ff300a9c8efab7741bfb82620e
Helio <helio.miranda.n@gmail.com>
2024-10-08 19:21:38 -0300

    WIP: Criando o formulário de cadastro do alunos.

--- src/app/pages/alunos/cadastrar-alunos/cadastrar-alunos.component.ts
+++ src/app/pages/alunos/cadastrar-alunos/cadastrar-alunos.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { SideBarComponent } from '../../central/side-bar/side-bar.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastrar-alunos',
   standalone: true,
   imports: [
-    SideBarComponent
+    SideBarComponent,
+    ReactiveFormsModule
   ],
   templateUrl: './cadastrar-alunos.component.html',
   styleUrl: './cadastrar-alunos.component.css'
